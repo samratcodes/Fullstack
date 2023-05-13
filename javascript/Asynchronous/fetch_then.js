44d8c2c21b8b58222304aef5331a0ec02fb3ab95
samratcodess <samratcodess@gmail.com>
2023-05-13 15:28:22 +0545

    feat:used api data

---
+++ javascript/Asynchronous/fetch_then.js
@@ -0,0 +1,46 @@
+fetch("https://dummyjson.com/products")
+  .then((response) => {
+ 
+    return response.json();
+  })
+  .then((data) => {
+
+
+    const container = document.getElementById('container');
+    container.className = "d-flex justify-content-around flex-wrap";
+
+  
+
+    data.products.forEach((product) => {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.style.width = '18rem';
+
+      const image = document.createElement('img');
+      image.src = product.thumbnail;
+      image.className = 'h-50';
+      card.append(image);
+
+      const body = document.createElement('div');
+      body.className = 'card-body';
+      card.append(body);
+
+      const title = document.createElement('h5');
+      title.className = 'card-title';
+      title.innerHTML = product.title;
+      card.append(title);
+
+      const description = document.createElement('p');
+      description.className = 'card-text';
+      description.innerHTML = product.description;
+      card.append(description);
+
+      const button = document.createElement('a');
+      button.className = 'btn btn-primary';
+      button.innerHTML = 'View product';
+      card.append(button);
+
+      container.append(card);
+    });
+  })
+  
